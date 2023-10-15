c36341359a201135036bfc43422bb94ce2a91e91
João Paulo Mazzo <mazzojp@gmail.com>
2023-10-15 09:03:17 -0300

    aula sobre heranças

---
+++ fundamentosJava/programacaoOrientadaObjetos/Aluno.java
@@ -0,0 +1,15 @@
+package programacaoOrientadaObjetos;
+
+public class Aluno extends Pessoa {
+    
+    private String matricula;
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+}
--- fundamentosJava/programacaoOrientadaObjetos/TesteDaPessoa.java
+++ fundamentosJava/programacaoOrientadaObjetos/TesteDaPessoa.java
@@ -7,15 +7,21 @@ public class TesteDaPessoa {
         pessoa1.setCpf("48268869852");
         pessoa1.setNome("João Paulo Mazzo");
         pessoa1.setIdade(23);
-
         System.out.println(pessoa1.toString());
 
-        Pessoa pessoa2 = new Pessoa();
-        pessoa2.setCpf("0723216869");
-        pessoa2.setNome("Paulo Mazzo");
-        pessoa2.setIdade(58);
+        Professor professor = new Professor();
+        professor.setCpf("0723216869");
+        professor.setNome("Paulo Mazzo");
+        professor.setIdade(58);
+        professor.setSalario(3000);
+        System.out.println(professor.toString() + professor.getSalario());
 
-        System.out.println(pessoa2.toString());
+        Aluno aluno = new Aluno();
+        aluno.setCpf("0723216869");
+        aluno.setNome("Paulo Mazzo");
+        aluno.setIdade(58);
+        aluno.setMatricula("123a");
+        System.out.println(aluno.toString() + aluno.getMatricula());
     }
 
 }
