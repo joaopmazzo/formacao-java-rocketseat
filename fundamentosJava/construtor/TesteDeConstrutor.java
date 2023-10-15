afb55fe44ad6a2867da270bac4c13f22f7428369
João Paulo Mazzo <mazzojp@gmail.com>
2023-10-15 16:02:49 -0300

    aula sobre construtor

---
+++ fundamentosJava/construtor/TesteDeConstrutor.java
@@ -0,0 +1,10 @@
+package construtor;
+
+public class TesteDeConstrutor {
+
+    public static void main(String[] args) {
+        Construtor construtor = new Construtor(0);
+        System.out.println(construtor.getNumero());
+    }
+    
+}
