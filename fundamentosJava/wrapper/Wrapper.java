ba2d311b5a0d63debc045029eeea0ffd0986ba3a
João Paulo Mazzo <mazzojp@gmail.com>
2023-10-15 17:03:47 -0300

    aula sobre wrapper classes

---
+++ fundamentosJava/wrapper/Wrapper.java
@@ -0,0 +1,15 @@
+package wrapper;
+
+public class Wrapper {
+
+    public static void main(String[] args) {
+        int tipoInt = 1;
+        float tipoFloat = 1;
+        boolean tipoBoolean = true;
+
+        Integer tipoInteger = 1;
+        Float tipoFloat2 = 1F;
+        Boolean tipoBoolean2 = false;
+    }
+
+}
