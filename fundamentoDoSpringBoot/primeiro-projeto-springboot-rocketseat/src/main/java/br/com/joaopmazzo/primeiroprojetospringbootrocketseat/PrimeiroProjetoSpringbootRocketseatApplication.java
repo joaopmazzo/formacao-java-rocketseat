87158fefb999f7413cb515ddaea64c30f204219e
João Paulo Mazzo <mazzojp@gmail.com>
2023-10-19 20:53:44 -0300

    aula sobre como criar um projeto springboot

---
+++ fundamentoDoSpringBoot/primeiro-projeto-springboot-rocketseat/src/main/java/br/com/joaopmazzo/primeiroprojetospringbootrocketseat/PrimeiroProjetoSpringbootRocketseatApplication.java
@@ -0,0 +1,13 @@
+package br.com.joaopmazzo.primeiroprojetospringbootrocketseat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroProjetoSpringbootRocketseatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroProjetoSpringbootRocketseatApplication.class, args);
+	}
+
+}
---
+++ fundamentoDoSpringBoot/primeiro-projeto-springboot-rocketseat/src/test/java/br/com/joaopmazzo/primeiroprojetospringbootrocketseat/PrimeiroProjetoSpringbootRocketseatApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joaopmazzo.primeiroprojetospringbootrocketseat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroProjetoSpringbootRocketseatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
