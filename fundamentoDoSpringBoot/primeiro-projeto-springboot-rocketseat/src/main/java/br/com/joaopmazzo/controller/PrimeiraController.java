f29f77ec12b4b8a37cf7675173b95dc3ef0ce677
João Paulo Mazzo <mazzojp@gmail.com>
2023-10-20 10:36:37 -0300

    aula 'Path Params'

--- fundamentoDoSpringBoot/primeiro-projeto-springboot-rocketseat/src/main/java/br/com/joaopmazzo/controller/PrimeiraController.java
+++ fundamentoDoSpringBoot/primeiro-projeto-springboot-rocketseat/src/main/java/br/com/joaopmazzo/controller/PrimeiraController.java
@@ -1,16 +1,30 @@
-package br.com.joaopmazzo.primeiroprojetospringbootrocketseat.controller;
+package br.com.joaopmazzo.controller;
+
+import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/primeiraController")
 public class PrimeiraController {
 
-    @GetMapping("/primeiroMetodo")
-    public String primeiroMetodo() {
-        return "teste de retorno get";
+    @GetMapping("/primeiroMetodo/{id}")
+    public String primeiroMetodo(@PathVariable Long id) {
+        return "O parametro é " + id;
+    }
+
+    @GetMapping("/metodoComQueryParams/{id}")
+    public String metodoComQueryParams(@RequestParam Long id) {
+        return "O parametro com metodoComQueryParams é " + id;
+    }
+
+    @GetMapping("/metodoComQueryParams2/{id}")
+    public String metodoComQueryParams2(@RequestParam Map<String, String> allParams) {
+        return "O parametro com metodoComQueryParams2 é " + allParams.entrySet();
     }
     
 }
