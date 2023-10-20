9f06d1d9b8bb4c517c8f0a8a6ba37aaac6f06410
João Paulo Mazzo <mazzojp@gmail.com>
2023-10-20 14:17:05 -0300

    Aula 'Ioc Di'

---
+++ fundamentoDoSpringBoot/primeiro-projeto-springboot-rocketseat/src/main/java/br/com/joaopmazzo/ioc_di/MeuComponent.java
@@ -0,0 +1,12 @@
+package br.com.joaopmazzo.ioc_di;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MeuComponent {
+    
+    public String chamarMeuComponent() {
+        return "Chamando meu component";
+    }
+
+}
