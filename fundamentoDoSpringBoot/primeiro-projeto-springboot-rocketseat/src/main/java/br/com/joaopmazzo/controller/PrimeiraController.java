4a64d6905ff0e2b64fdd4adad7eebf5879f22384
João Paulo Mazzo <mazzojp@gmail.com>
2023-10-20 13:44:34 -0300

    aula 'Header Params'

--- fundamentoDoSpringBoot/primeiro-projeto-springboot-rocketseat/src/main/java/br/com/joaopmazzo/controller/PrimeiraController.java
+++ fundamentoDoSpringBoot/primeiro-projeto-springboot-rocketseat/src/main/java/br/com/joaopmazzo/controller/PrimeiraController.java
@@ -4,6 +4,9 @@ import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,5 +29,22 @@ public class PrimeiraController {
     public String metodoComQueryParams2(@RequestParam Map<String, String> allParams) {
         return "O parametro com metodoComQueryParams2 é " + allParams.entrySet();
     }
+
+    @PostMapping("/metodoComBodyParams")
+    public String metodoComBodyParams(@RequestBody Usuario usuario) {
+        return "metodoComBodyParams " + usuario.username();
+    }
+
+    @PostMapping("/metodoComHeaders")
+    public String metodoComHeaders(@RequestHeader("name") String name) {
+        return "metodoComHeaders " + name;
+    }
+
+    @PostMapping("/metodoComListHeaders")
+    public String metodoComListHeaders(@RequestHeader Map<String, String> headers) {
+        return "metodoComListHeaders " + headers.entrySet();
+    }
+
+    record Usuario(String username) {}
     
 }
