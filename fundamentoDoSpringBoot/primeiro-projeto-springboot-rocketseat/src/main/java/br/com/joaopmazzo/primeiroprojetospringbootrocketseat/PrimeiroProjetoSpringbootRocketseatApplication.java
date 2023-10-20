48186a2b5ad53ebaa5bfdd4dcbc88ba0208f535d
João Paulo Mazzo <mazzojp@gmail.com>
2023-10-20 10:28:59 -0300

    aula 'Component Scan'

--- fundamentoDoSpringBoot/primeiro-projeto-springboot-rocketseat/src/main/java/br/com/joaopmazzo/primeiroprojetospringbootrocketseat/PrimeiroProjetoSpringbootRocketseatApplication.java
+++ fundamentoDoSpringBoot/primeiro-projeto-springboot-rocketseat/src/main/java/br/com/joaopmazzo/primeiroprojetospringbootrocketseat/PrimeiroProjetoSpringbootRocketseatApplication.java
@@ -2,8 +2,10 @@ package br.com.joaopmazzo.primeiroprojetospringbootrocketseat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "br.com.joapmazzo")
 public class PrimeiroProjetoSpringbootRocketseatApplication {
 
 	public static void main(String[] args) {
